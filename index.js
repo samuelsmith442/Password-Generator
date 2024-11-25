8f953582a8d6224a019f48ced2c6135a4579deb4
Samuel Smith <10069706+samuelsmith442@users.noreply.github.com>
2024-11-25 05:55:26 -0500

    update code base
    app updates for responsiveness and improved the javascript file

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_-+={[}]|:;<>.?/".split("");
 
 const passwordLength = 15;
 
-function getRandomCharacter() {
-    const randomChar = Math.floor(Math.random() * characters.length);
-    return characters[randomChar];
+function getRandomCharacter(charSet) {
+    const randomChar = Math.floor(Math.random() * charSet.length);
+    return charSet[randomChar];
+}
+
+function ensureCharacterTypes(password) {
+    if (!/[A-Z]/.test(password)) {
+        password = password.replace(/./, getRandomCharacter("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+    }
+    if (!/[a-z]/.test(password)) {
+        password = password.replace(/./, getRandomCharacter("abcdefghijklmnopqrstuvwxyz"));
+    }
+    if (!/[0-9]/.test(password)) {
+        password = password.replace(/./, getRandomCharacter("0123456789"));
+    }
+    if (!/[~`!@#$%^&*()_+\-=\{\}\[\]|:;<>.?/]/.test(password)) {
+        password = password.replace(/./, getRandomCharacter("~`!@#$%^&*()_+\-=\{\}\[\]|:;<>.?/"));
+    }
+    return password;
 }
 
 function generateRandomPassword() {
     let randomPassword = "";
     for (let i = 0; i < passwordLength; i++) {
-        randomPassword += getRandomCharacter();
+        randomPassword += getRandomCharacter(characters);
     }
-    return randomPassword;
+    return ensureCharacterTypes(randomPassword);
+}
+
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        alert("Password copied to clipboard!");
+    }).catch(err => {
+        console.error("Failed to copy: ", err);
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -27,5 +51,16 @@ document.addEventListener("DOMContentLoaded", function () {
         passwordDisplay1.value = generatedPassword1;
         passwordDisplay2.value = generatedPassword2;
     });
-});
 
+    passwordDisplay1.addEventListener("click", function () {
+        copyToClipboard(passwordDisplay1.value);
+    });
+
+    passwordDisplay2.addEventListener("click", function () {
+        copyToClipboard(passwordDisplay2.value);
+    });
+
+    generateButton.setAttribute("aria-label", "Generate Passwords");
+    passwordDisplay1.setAttribute("aria-label", "Generated Password 1");
+    passwordDisplay2.setAttribute("aria-label", "Generated Password 2");
+});
